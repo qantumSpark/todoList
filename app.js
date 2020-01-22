784b6d6ee5ef6664592e5f10422b7a1e0fa9f038
Antoine <pezetantoine.pro@gmail.com>
2020-01-22 17:05:54 +0100

    final build, maybe

--- app.js
+++ app.js
@@ -188,9 +188,9 @@ app.get("/about", function(req, res) {
 //Server port
 let port = process.env.PORT;
 if (port == null || port == "") {
-  port = 8000;
+  port = 3000;
 }
-app.listen(port);
+
 
 app.listen(port, function() {
   console.log("Server started on port :"+port);
