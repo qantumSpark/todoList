88fb84e3c02db7010437398034ea705744f45c92
Antoine <pezetantoine.pro@gmail.com>
2020-01-22 16:59:56 +0100

    Fix Port bug

--- app.js
+++ app.js
@@ -186,7 +186,12 @@ app.get("/about", function(req, res) {
 
 
 //Server port
-const port = process.env.port || 3000
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
+
 app.listen(port, function() {
   console.log("Server started on port :"+port);
 });
